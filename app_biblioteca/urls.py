8671a29c79d498146bea42abc7b4c9bbd18db741
Erick Amorim <erickamorimcarv@gmail.com>
2022-04-07 14:28:52 -0300

    Projeto Finalizado

--- app_biblioteca/urls.py
+++ app_biblioteca/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from app_biblioteca.views import IndexView, CreateLivroView, UpdateLivroView, DeleteLivroView
+from app_biblioteca.views import IndexView, CreateLivroView, UpdateLivroView, DeleteLivroView, DetailLivroView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create/', CreateLivroView.as_view(), name='create_livro'),
     path('<int:pk>/update/', UpdateLivroView.as_view(), name='update_livro'),
     path('<int:pk>/delete/', DeleteLivroView.as_view(), name='delete_livro'),
+    path('<int:pk>/detail/', DetailLivroView.as_view(), name='detail_livro'),
 ]
